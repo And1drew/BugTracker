649ed2da7a32502352a3c9649b15d0db29525e82
Andrew Belanger <andrew.j.belanger@gmail.com>
2020-08-30 08:03:23 -0400

    cleaned up commented out code

--- BugTrackApp/forms.py
+++ BugTrackApp/forms.py
@@ -10,14 +10,3 @@ class login_form(forms.Form):
 class new_ticket_form(forms.Form):
     title = forms.CharField(max_length = 80)
     description = forms.CharField(widget=forms.Textarea)
-    STATUS_CHOICES = [
-        ('New', 'New'),
-        ('In Progress', 'In Progress'),
-        ('Done', 'Done'),
-        ('Invalid', 'Invalid'),
-    ]
-    # status = forms.ChoiceField(choices=STATUS_CHOICES, initial='New')
-    # assigned = forms.ModelChoiceField(queryset = custom_user.objects.all(), blank = True, initial = None)
-    # created_by = forms.ForeignKey(custom_user, on_delete=forms.DO_NOTHING)
-    # date = forms.DateTimeField(default=datetime.now)
-    # completed = forms.CharField(default = None, null = True, max_length = 30)
